ad9f316d98318b4c5ef4874728c44d86f7e0d5cc
matias4205 <matiasperezpc@hotmail.com>
2019-12-11 20:24:08 -0300

    Adding webpack for react development

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Panel from './pages/Panel';
+import Client from './pages/Client';
+
+const App = () => {
+    return (
+        <Client/>
+    );
+};
+
+export default App;
---
+++ frontend/src/pages/Client/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../../assets/styles/Client.scss';
+
+class Client extends Component {
+    render() {
+        return (
+            <div>
+                d
+            </div>
+        );
+    }
+}
+
+export default Client;
